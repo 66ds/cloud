ff9d857d56423f315497cf7b9d02eeeba6405972
1532498760@qq.com <1532498760@qq.com>
2025-07-17 17:35:12 +0800

    更新了一些功能

---
+++ blog-websocket/src/main/java/com/qianbing/websocket/BlogWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.qianbing.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogWebsocketApplication.class, args);
+    }
+
+}
